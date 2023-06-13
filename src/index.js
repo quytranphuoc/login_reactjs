35ab91247cc3746aedb16718aa21bb0062b8c449
git-training <quyt40247@gmail.com>
2023-06-13 16:32:56 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './AppRouter';
-
+import App from './App';
 ReactDOM.render(
   <React.StrictMode>
-    <AppRouter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -12,4 +11,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
         alert(`Email: ${email}\nPassword: ${password}`);
